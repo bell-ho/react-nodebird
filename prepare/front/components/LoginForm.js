29cc0787cb88b1b1c704513bebc3d15747f9e91a
jongho <jj0101065@gmail.com>
2022-05-27 02:14:35 +0900

    로그인 로그아웃 구현

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -1,18 +1,38 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
+import styled from 'styled-components';
 
-const LoginForm = () => {
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
+  // 이런식으로 스타일 가능
+  // const style = useMemo(() => ({ marginTop: 10 }), []);
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+
   const onChangeId = useCallback((e) => {
     setId(e.target.value);
   }, []);
+
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
-  }, []);
+  });
+
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
+    setIsLoggedIn(true);
+    // state는 넣어줘야함 변하는 것이여서 (빈 배열로 넣으면 마운트 시점에 id와 password가 없어서 같은걸로 쳐서 리랜더링이 안됨)
+  }, [id, password]);
+
+  const FormWrapper = styled(Form)`
+    padding: 10px;
+  `;
+
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -28,7 +48,7 @@ const LoginForm = () => {
           required
         />
       </div>
-      <div>
+      <ButtonWrapper>
         <Button type="primary" htmlType="submit" loading={false}>
           로그인
         </Button>
@@ -37,8 +57,8 @@ const LoginForm = () => {
             <Button>회원가입</Button>
           </a>
         </Link>
-      </div>
-    </Form>
+      </ButtonWrapper>
+    </FormWrapper>
   );
 };
 
--- prepare/front/components/UserProfile.js
+++ prepare/front/components/UserProfile.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Avatar, Button, Card } from 'antd';
 
-const UserProfile = () => {
-  return <div></div>;
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogOut = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          쨱짹 <br />0
+        </div>,
+        <div key="followings">
+          팔로잉 <br />0
+        </div>,
+        <div key="followings">
+          팔로잉 <br />0
+        </div>,
+      ]}
+    >
+      <Card.Meta title="bell-ho" avatar={<Avatar>JH</Avatar>} />
+      <Button onClick={onLogOut}>로그아웃</Button>
+    </Card>
+  );
 };
 
 export default UserProfile;

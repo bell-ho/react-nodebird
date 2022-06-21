1e42a44ae62a5a2ff180a7edb42eae81ed094c50
jongho <jh01010@doctorsoft.co.kr>
2022-06-21 17:46:42 +0900

    팔로잉한 사람들만 가져오기

--- prepare/back/routes/posts.js
+++ prepare/back/routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Post, Image, User, Comment } = require("../models");
 const router = express.Router();
 const { Op } = require("sequelize");
+const { Sequelize } = require("sequelize");
 router.get("/", async (req, res, next) => {
   try {
     const where = {};
@@ -57,4 +58,142 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get("/related", async (req, res, next) => {
+  try {
+    const followings = await User.findAll({
+      attributes: ["id"],
+      include: [
+        {
+          model: User,
+          as: "Followers", //사용자들 중 팔로워에 내가 있는 경우
+          where: { id: req.user.id },
+        },
+      ],
+    });
+    const where = {
+      UserId: { [Op.in]: followings.map((v) => v.id) },
+    };
+    if (parseInt(req.query.lastId, 10)) {
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }; //보다 작은
+    }
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [
+        ["createdAt", "DESC"],
+        [Comment, "createdAt", "DESC"],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+              order: [["createdAt", "DESC"]],
+            },
+          ],
+        },
+        {
+          model: User, // 좋아요 누른사람
+          as: "Likers",
+          attributes: ["id"],
+        },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            { model: Image },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/unrelated", async (req, res, next) => {
+  try {
+    const followings = await User.findAll({
+      attributes: ["id"],
+      include: [
+        {
+          model: User,
+          as: "Followers", //사용자들 중 팔로워에 내가 있는 경우
+          where: { id: req.user.id },
+        },
+      ],
+    });
+    // 차단 관계도 만들 수 있음 model Ignored
+    const where = {
+      UserId: { [Op.notIn]: followings.map((v) => v.id).concat(req.user.id) },
+    };
+    if (parseInt(req.query.lastId, 10)) {
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }; //보다 작은
+    }
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [
+        ["createdAt", "DESC"],
+        [Comment, "createdAt", "DESC"],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+              order: [["createdAt", "DESC"]],
+            },
+          ],
+        },
+        {
+          model: User, // 좋아요 누른사람
+          as: "Likers",
+          attributes: ["id"],
+        },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            { model: Image },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;

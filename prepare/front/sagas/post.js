c5fa0e45e1e46845ae3790743efd00f57c7d706c
jongho <jj0101065@gmail.com>
2022-06-25 19:43:14 +0900

    aws 람다로 이미지 리사이징

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -294,15 +294,15 @@ function* watchUnLikePost() {
 }
 
 function* watchLoadPosts() {
-  yield throttle(500, LOAD_POSTS_REQUEST, loadPosts);
+  yield throttle(1000, LOAD_POSTS_REQUEST, loadPosts);
 }
 
 function* watchLoadUserPosts() {
-  yield throttle(500, LOAD_USER_POSTS_REQUEST, loadUserPosts);
+  yield throttle(1000, LOAD_USER_POSTS_REQUEST, loadUserPosts);
 }
 
 function* watchLoadHashtagPosts() {
-  yield throttle(500, LOAD_HASHTAG_POSTS_REQUEST, loadHashtagPosts);
+  yield throttle(1000, LOAD_HASHTAG_POSTS_REQUEST, loadHashtagPosts);
 }
 
 function* watchLoadPost() {

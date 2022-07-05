bdcba5775cf47c7b6f13d56a9846c7ab04a36d0c
jongho <jj0101065@gmail.com>
2022-07-05 21:41:45 +0900

    google

--- prepare/back/passport/google.js
+++ prepare/back/passport/google.js
@@ -1,36 +1,8 @@
-const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const callbackUrl = require("../config/callbackUrl");
 
-const session = require("express-session");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
-
-const MySQLStore = require("express-mysql-session")(session);
-
-const options = {
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
-
-// mysql session store 생성
-const sessionStore = new MySQLStore(options);
-
-app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 dotenv.config();
 const { User } = require("../models");
@@ -42,7 +14,7 @@ module.exports = () => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: callbackUrl,
-        passReqToCallback: true,
+        // passReqToCallback: true,
         // callbackURL: "http://localhost:3065/user/auth/google/callback/",
       },
       async (accessToken, refreshToken, profile, done) => {
